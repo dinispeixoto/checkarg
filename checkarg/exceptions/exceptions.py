b935c7b8174767084ce1682df5efff9756929583
Dinis Peixoto <dinis.peixoto@farfetch.com>
2021-02-28 19:09:13 +0000

    feat: Add custom exception messages
    - Raise ValueError instead of generic Exception
    - Create Custom Exceptions for each validation
    - Update folder structure

---
+++ checkarg/exceptions/exceptions.py
@@ -0,0 +1,22 @@
+from .error_messages import DefaultErrorMessages
+
+# ValueError instead of Exception
+# ValueError(Exception) but can be trapped alone
+# i.e. except(ValueError) is more useful than except(Exception)
+
+
+class ArgumentError(ValueError):
+    def __init__(self, message, argument_name):
+        self.message = (
+            message
+            if message
+            else DefaultErrorMessages.default_argument_message(argument_name)
+        )
+
+
+class ArgumentNoneError(ArgumentError):
+    pass
+
+
+class ArgumentOutOfRangeError(ArgumentError):
+    pass
